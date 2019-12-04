b520f54b4ba2791600772716c5a35bbe54858e4e
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-12-04 14:13:17 +0900

    custom config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // Replace this section with the config file of your own Firebase project. 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWrrXYmTGmwUnTzevw7NQ5_3yFOPUb9Ns",
-  authDomain: "blog-us.firebaseapp.com",
-  databaseURL: "https://blog-us.firebaseio.com",
-  projectId: "blog-us",
-  storageBucket: "blog-us.appspot.com",
-  messagingSenderId: "1091642071033",
-  appId: "1:1091642071033:web:bad75989863759db"
+  apiKey: "AIzaSyDIRRdPkeCQVniv22YDXH6oA1oW7AOPwI4",
+  authDomain: "firepresso.firebaseapp.com",
+  databaseURL: "https://firepresso.firebaseio.com",
+  projectId: "firepresso",
+  storageBucket: "firepresso.appspot.com",
+  messagingSenderId: "351087874664",
+  appId: "1:351087874664:web:ad9ecba660715261e861dd"
 };
 
 export default firebaseConfig;
