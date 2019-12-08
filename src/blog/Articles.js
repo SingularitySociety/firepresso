dd186e8c129c4ef0c1771ffb58df90d5d0d52b43
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-12-07 18:50:58 -0800

    list

--- src/blog/Articles.js
+++ src/blog/Articles.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 //import ArticleList from './ArticleList';
@@ -15,6 +15,24 @@ const styles = theme => ({
 function Articles(props) {
   const { db, user } = props;
   const [redirect, setRedirect] = useState(null);
+  const [list, setList] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+    let query = db.collection(`users/${user.uid}/articles`).orderBy("created", "desc");
+    const detacher = query.onSnapshot((snapshot) => {
+      const list = [];
+      snapshot.forEach((doc)=>{
+        const article = doc.data();
+        article.articleId = doc.id;
+        list.push(article);
+      });
+      setList(list);
+    }, (e) => {
+      setError(e);
+    });
+    return detacher;
+  }, [db]);
 
   const createArticle = async (title) => {
     const doc = await db.collection(`users/${user.uid}/articles`).add({
@@ -29,6 +47,7 @@ function Articles(props) {
   if (redirect) {
     return <Redirect to={redirect} />
   }
+  console.log(list);
   //const context = { user, db };
   return (
     <Grid container justify="center" spacing={1}>
@@ -37,6 +56,12 @@ function Articles(props) {
             action={<FormattedMessage id="create" />} label={<FormattedMessage id="article" />}/>
       </Grid>
       <Grid item xs={12}>
+      {
+        list.map((article)=>{
+          return <p key={article.articleId}>{article.title}</p>
+          //return <ArticleItem key={article.articleId} article={article} {...context} />
+        })
+      }
       </Grid>
     </Grid>
   )
